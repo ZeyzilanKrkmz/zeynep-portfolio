032a30690e274b32eeb17a90639d71208c7f68f9
ZeyzilanKrkmz <zzk603061@gmail.com>
2025-05-23 16:47:19 +0300

    form eklendi

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Hero.module.css";
 import { getImageUrl } from "../../utils";
 
 export const Hero = () => {
+  const [showForm, setShowForm] = useState(false);
+
+  const toggleForm = () => {
+    setShowForm((prev) => !prev);
+  };
+
   return (
     <section id="hero" className={styles.container}>
       <div className={styles.content}>
@@ -10,10 +16,37 @@ export const Hero = () => {
         <p className={styles.description}>
           Bilgisayar mühendisliği öğrencisiyim.
         </p>
-        <a href="#" className={styles.contactButton}>
+        <button onClick={toggleForm} className={styles.contactButton}>
           Benimle iletişime geçin.
-        </a>
+        </button>
+
+        {showForm && (
+          <form className={styles.contactForm}>
+            <input
+              type="text"
+              placeholder="Ad Soyad"
+              className={styles.input}
+              required
+            />
+            <input
+              type="email"
+              placeholder="Email"
+              className={styles.input}
+              required
+            />
+            <textarea
+              placeholder="İçerik"
+              className={styles.textarea}
+              rows="4"
+              required
+            ></textarea>
+            <button type="submit" className={styles.submitButton}>
+              Gönder
+            </button>
+          </form>
+        )}
       </div>
+
       <img
         src={getImageUrl("hero/IMG-20250520-WA0025.webp")}
         alt="hero image of me"
